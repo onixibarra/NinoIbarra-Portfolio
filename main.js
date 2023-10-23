722fb327d3f578c7659b8c60aa1f04e5864c2ec7
Nino Ibarra <mrninoibarra@gmail.com>
2023-10-23 12:33:27 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Nino Ibarra Page</title>
+<!-- Linking to an external CSS file for styling -->
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <header>
+    <!-- Main header section with my name and title -->
+        <h1>Nino Ibarra</h1>
+        <h2>Web Developer</h2>
+   
+    <!-- image of me -->
+        <img src="profile.jpg" alt="Nino Ibarra" width="200" height="200">
+    </header>
+
+<!-- main section of the page -->
+    <main>
+        <section id="intro">
+            <h3>Hello! I'm Nino Ibarra</h3>
+            <p>I am a web developer and a business professor living in Sterling Heights, Michigan.</p>
+        </section>
+
+<!-- line break -->
+        <hr>
+
+<!-- about me section. a little background. -->
+        <section id="about">
+            <h3>About Me</h3>
+            <p>I am passionate about merging academic knowledge with real-world expertise to empower students and businesses alike in navigating the digital landscape.</p>
+            <p>Outside of web development and teaching, I enjoy biking, hiking, and camping. I also dabble a little bit in the culinary arts.</p>
+        </section>
+
+<!-- Another line break -->
+        <hr>
+
+<!-- contact information listing 3 different ways to get in touch with me -->
+        <section id="contact">
+            <h3>Contact Me</h3>
+            <p>I'd love to hear from you! Please feel free to contact or follow me:</p>
+            <ul>
+                <li><a href="https://www.linkedin.com/in/nino-ibarra">LinkedIn</a></li>
+                <li><a href="https://github.com/ninoibarra">GitHub</a></li>
+                <li><a href="mailto:mrninoibarra@gmail.com">Email</a></li>
+            </ul>
+        </section>
+    </main>
+
+<!-- copyright footer-->
+    <footer>
+        <p>&copy; 2023 Nino Ibarra</p>
+        <p>Website designed by Nino Ibarra</p>
+    </footer>
+</body>
+</html>
